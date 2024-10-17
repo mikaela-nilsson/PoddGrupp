06b061cf02a4ffa5ebdc590091bac41541ec7f56
mikaela-nilsson <mdjnilsson@gmail.com>
2024-10-17 13:47:22 +0200

    första design

--- PoddGrupp/PoddcastVisare.Designer.cs
+++ PoddGrupp/PoddcastVisare.Designer.cs
@@ -28,13 +28,301 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblPoddcast = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblNamnPaFlodet = new System.Windows.Forms.Label();
+            this.lblRSSLank = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cbKategori = new System.Windows.Forms.ComboBox();
+            this.lblKategori = new System.Windows.Forms.Label();
+            this.btnLaggTill = new System.Windows.Forms.Button();
+            this.btnAndra = new System.Windows.Forms.Button();
+            this.btnTaBort = new System.Windows.Forms.Button();
+            this.listaFloden = new System.Windows.Forms.ListBox();
+            this.lblKategorier = new System.Windows.Forms.Label();
+            this.listaKategorier = new System.Windows.Forms.ListBox();
+            this.btnTaBortKategori = new System.Windows.Forms.Button();
+            this.btnAndraKategori = new System.Windows.Forms.Button();
+            this.btnLaggTillKategori = new System.Windows.Forms.Button();
+            this.tbKategori = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblAvsnitt = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listaAvsnitt = new System.Windows.Forms.ListBox();
+            this.btnAterstall = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPoddcast
+            // 
+            this.lblPoddcast.AutoSize = true;
+            this.lblPoddcast.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPoddcast.ForeColor = System.Drawing.Color.Sienna;
+            this.lblPoddcast.Location = new System.Drawing.Point(518, 9);
+            this.lblPoddcast.Name = "lblPoddcast";
+            this.lblPoddcast.Size = new System.Drawing.Size(143, 44);
+            this.lblPoddcast.TabIndex = 0;
+            this.lblPoddcast.Text = "Poddcast";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(183, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // lblNamnPaFlodet
+            // 
+            this.lblNamnPaFlodet.AutoSize = true;
+            this.lblNamnPaFlodet.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNamnPaFlodet.Location = new System.Drawing.Point(24, 157);
+            this.lblNamnPaFlodet.Name = "lblNamnPaFlodet";
+            this.lblNamnPaFlodet.Size = new System.Drawing.Size(153, 28);
+            this.lblNamnPaFlodet.TabIndex = 3;
+            this.lblNamnPaFlodet.Text = "Namn på flödet:";
+            // 
+            // lblRSSLank
+            // 
+            this.lblRSSLank.AutoSize = true;
+            this.lblRSSLank.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRSSLank.Location = new System.Drawing.Point(72, 104);
+            this.lblRSSLank.Name = "lblRSSLank";
+            this.lblRSSLank.Size = new System.Drawing.Size(105, 28);
+            this.lblRSSLank.TabIndex = 5;
+            this.lblRSSLank.Text = "RSS Länk:";
+            this.lblRSSLank.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(183, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(827, 26);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // cbKategori
+            // 
+            this.cbKategori.FormattingEnabled = true;
+            this.cbKategori.Location = new System.Drawing.Point(183, 211);
+            this.cbKategori.Name = "cbKategori";
+            this.cbKategori.Size = new System.Drawing.Size(162, 28);
+            this.cbKategori.TabIndex = 6;
+            // 
+            // lblKategori
+            // 
+            this.lblKategori.AutoSize = true;
+            this.lblKategori.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKategori.Location = new System.Drawing.Point(84, 211);
+            this.lblKategori.Name = "lblKategori";
+            this.lblKategori.Size = new System.Drawing.Size(93, 28);
+            this.lblKategori.TabIndex = 7;
+            this.lblKategori.Text = "Kategori:";
+            // 
+            // btnLaggTill
+            // 
+            this.btnLaggTill.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLaggTill.Location = new System.Drawing.Point(59, 545);
+            this.btnLaggTill.Name = "btnLaggTill";
+            this.btnLaggTill.Size = new System.Drawing.Size(98, 38);
+            this.btnLaggTill.TabIndex = 8;
+            this.btnLaggTill.Text = "Lägg Till";
+            this.btnLaggTill.UseVisualStyleBackColor = true;
+            // 
+            // btnAndra
+            // 
+            this.btnAndra.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAndra.Location = new System.Drawing.Point(264, 545);
+            this.btnAndra.Name = "btnAndra";
+            this.btnAndra.Size = new System.Drawing.Size(81, 38);
+            this.btnAndra.TabIndex = 9;
+            this.btnAndra.Text = "Ändra";
+            this.btnAndra.UseVisualStyleBackColor = true;
+            // 
+            // btnTaBort
+            // 
+            this.btnTaBort.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTaBort.Location = new System.Drawing.Point(166, 545);
+            this.btnTaBort.Name = "btnTaBort";
+            this.btnTaBort.Size = new System.Drawing.Size(81, 38);
+            this.btnTaBort.TabIndex = 10;
+            this.btnTaBort.Text = "Ta Bort";
+            this.btnTaBort.UseVisualStyleBackColor = true;
+            // 
+            // listaFloden
+            // 
+            this.listaFloden.BackColor = System.Drawing.SystemColors.Window;
+            this.listaFloden.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listaFloden.FormattingEnabled = true;
+            this.listaFloden.ItemHeight = 28;
+            this.listaFloden.Location = new System.Drawing.Point(387, 159);
+            this.listaFloden.Name = "listaFloden";
+            this.listaFloden.Size = new System.Drawing.Size(623, 424);
+            this.listaFloden.TabIndex = 11;
+            // 
+            // lblKategorier
+            // 
+            this.lblKategorier.AutoSize = true;
+            this.lblKategorier.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKategorier.ForeColor = System.Drawing.Color.Sienna;
+            this.lblKategorier.Location = new System.Drawing.Point(84, 634);
+            this.lblKategorier.Name = "lblKategorier";
+            this.lblKategorier.Size = new System.Drawing.Size(163, 44);
+            this.lblKategorier.TabIndex = 15;
+            this.lblKategorier.Text = "Kategorier";
+            // 
+            // listaKategorier
+            // 
+            this.listaKategorier.BackColor = System.Drawing.SystemColors.Window;
+            this.listaKategorier.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listaKategorier.FormattingEnabled = true;
+            this.listaKategorier.ItemHeight = 28;
+            this.listaKategorier.Location = new System.Drawing.Point(73, 681);
+            this.listaKategorier.Name = "listaKategorier";
+            this.listaKategorier.Size = new System.Drawing.Size(365, 200);
+            this.listaKategorier.TabIndex = 14;
+            // 
+            // btnTaBortKategori
+            // 
+            this.btnTaBortKategori.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTaBortKategori.Location = new System.Drawing.Point(493, 800);
+            this.btnTaBortKategori.Name = "btnTaBortKategori";
+            this.btnTaBortKategori.Size = new System.Drawing.Size(81, 38);
+            this.btnTaBortKategori.TabIndex = 18;
+            this.btnTaBortKategori.Text = "Ta Bort";
+            this.btnTaBortKategori.UseVisualStyleBackColor = true;
+            // 
+            // btnAndraKategori
+            // 
+            this.btnAndraKategori.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAndraKategori.Location = new System.Drawing.Point(493, 681);
+            this.btnAndraKategori.Name = "btnAndraKategori";
+            this.btnAndraKategori.Size = new System.Drawing.Size(81, 38);
+            this.btnAndraKategori.TabIndex = 17;
+            this.btnAndraKategori.Text = "Ändra";
+            this.btnAndraKategori.UseVisualStyleBackColor = true;
+            // 
+            // btnLaggTillKategori
+            // 
+            this.btnLaggTillKategori.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLaggTillKategori.Location = new System.Drawing.Point(476, 740);
+            this.btnLaggTillKategori.Name = "btnLaggTillKategori";
+            this.btnLaggTillKategori.Size = new System.Drawing.Size(98, 38);
+            this.btnLaggTillKategori.TabIndex = 16;
+            this.btnLaggTillKategori.Text = "Lägg Till";
+            this.btnLaggTillKategori.UseVisualStyleBackColor = true;
+            // 
+            // tbKategori
+            // 
+            this.tbKategori.Location = new System.Drawing.Point(602, 687);
+            this.tbKategori.Name = "tbKategori";
+            this.tbKategori.Size = new System.Drawing.Size(146, 26);
+            this.tbKategori.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(602, 740);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(146, 26);
+            this.textBox3.TabIndex = 20;
+            // 
+            // lblAvsnitt
+            // 
+            this.lblAvsnitt.AutoSize = true;
+            this.lblAvsnitt.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAvsnitt.ForeColor = System.Drawing.Color.Sienna;
+            this.lblAvsnitt.Location = new System.Drawing.Point(1105, 104);
+            this.lblAvsnitt.Name = "lblAvsnitt";
+            this.lblAvsnitt.Size = new System.Drawing.Size(126, 44);
+            this.lblAvsnitt.TabIndex = 23;
+            this.lblAvsnitt.Text = "Avsnitt";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1059, 392);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(237, 200);
+            this.richTextBox1.TabIndex = 22;
+            this.richTextBox1.Text = "";
+            // 
+            // listaAvsnitt
+            // 
+            this.listaAvsnitt.BackColor = System.Drawing.SystemColors.Window;
+            this.listaAvsnitt.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listaAvsnitt.FormattingEnabled = true;
+            this.listaAvsnitt.ItemHeight = 28;
+            this.listaAvsnitt.Location = new System.Drawing.Point(1059, 159);
+            this.listaAvsnitt.Name = "listaAvsnitt";
+            this.listaAvsnitt.Size = new System.Drawing.Size(237, 200);
+            this.listaAvsnitt.TabIndex = 21;
+            // 
+            // btnAterstall
+            // 
+            this.btnAterstall.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAterstall.Location = new System.Drawing.Point(205, 256);
+            this.btnAterstall.Name = "btnAterstall";
+            this.btnAterstall.Size = new System.Drawing.Size(140, 38);
+            this.btnAterstall.TabIndex = 24;
+            this.btnAterstall.Text = "Återställ Filter";
+            this.btnAterstall.UseVisualStyleBackColor = true;
+            // 
+            // PoddcastVisare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(1365, 1050);
+            this.Controls.Add(this.btnAterstall);
+            this.Controls.Add(this.lblAvsnitt);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listaAvsnitt);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.tbKategori);
+            this.Controls.Add(this.btnTaBortKategori);
+            this.Controls.Add(this.btnAndraKategori);
+            this.Controls.Add(this.btnLaggTillKategori);
+            this.Controls.Add(this.lblKategorier);
+            this.Controls.Add(this.listaKategorier);
+            this.Controls.Add(this.listaFloden);
+            this.Controls.Add(this.btnTaBort);
+            this.Controls.Add(this.btnAndra);
+            this.Controls.Add(this.btnLaggTill);
+            this.Controls.Add(this.lblKategori);
+            this.Controls.Add(this.cbKategori);
+            this.Controls.Add(this.lblRSSLank);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblNamnPaFlodet);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblPoddcast);
+            this.Name = "PoddcastVisare";
+            this.Text = "Poddcast";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblPoddcast;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblNamnPaFlodet;
+        private System.Windows.Forms.Label lblRSSLank;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cbKategori;
+        private System.Windows.Forms.Label lblKategori;
+        private System.Windows.Forms.Button btnLaggTill;
+        private System.Windows.Forms.Button btnAndra;
+        private System.Windows.Forms.Button btnTaBort;
+        private System.Windows.Forms.ListBox listaFloden;
+        private System.Windows.Forms.Label lblKategorier;
+        private System.Windows.Forms.ListBox listaKategorier;
+        private System.Windows.Forms.Button btnTaBortKategori;
+        private System.Windows.Forms.Button btnAndraKategori;
+        private System.Windows.Forms.Button btnLaggTillKategori;
+        private System.Windows.Forms.TextBox tbKategori;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblAvsnitt;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listaAvsnitt;
+        private System.Windows.Forms.Button btnAterstall;
     }
 }
 
--- PoddGrupp/PoddcastVisare.cs
+++ PoddGrupp/PoddcastVisare.cs
@@ -16,5 +16,15 @@ namespace PoddGrupp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
